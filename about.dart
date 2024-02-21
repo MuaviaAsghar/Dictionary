a18ac2f90e973cb8f30607754af5fa7a3a189035
Muavia Asghar <newgamer445@gmail.com>
2024-02-21 20:23:30 +0500

    Completed till new idea and about updated

--- about.dart
+++ about.dart
@@ -1,7 +1,3 @@
 about() {
-  print('About Me');
-  print('------------');
-  print('Name: Muavia Asghar');
-  print(
-      "There is nothing cool about me and i made this app with some help of chat gpt.:)");
+  print("There is nothing to know about me.");
 }
