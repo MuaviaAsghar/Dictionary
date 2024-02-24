1525f47432966a3d1a647a9bd7f69fddcde3bf08
Muavia Asghar <newgamer445@gmail.com>
2024-02-24 23:57:09 +0500

    Completed till new idea and one if else condition replaced with ternary operator

--- main.dart
+++ main.dart
@@ -5,7 +5,6 @@ import 'about.dart';
 import 'help.dart';
 import 'printFullMeaning.dart';
 import 'history.dart';
-
 import 'showloading.dart';
 
 Future<void> main() async {
@@ -28,7 +27,7 @@ Future<void> main() async {
       "Tip => If you want to terminate the program after one function type **end in terminal**");
   print("");
 
-  stdout.write("Enter the function you want to use:");
+  stdout.write("Enter the name function you want to use:");
   String userInput = stdin.readLineSync()!;
   String lowercase = userInput.toString().toLowerCase();
 
@@ -114,99 +113,6 @@ Future<void> main() async {
 
       final rerun = stdin.readLineSync();
 
-      if (rerun == 'run') {
-        return main();
-      } else if (rerun == 'end') {
-        exit(0);
-      }
+      rerun == 'run' ? main() : (rerun == 'end' ? exit(0) : null);
   }
 }
-
- 
-    
- 
- 
-// if (number == '1') {
-//     print("");
-//     stdout.write("Enter the word for full meaning:");
-//     final word = stdin.readLineSync();
-//     showLoading();
-//     await Future.delayed(const Duration(seconds: 5));
-//     await meaning(word);
-//     await addToHistory(word);
-
-//     final rerun = stdin.readLineSync();
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-//   } else if (number == '2') {
-//     print("");
-//     stdout.write("Enter the word for full meaning:");
-//     final word = stdin.readLineSync();
-//     showLoading();
-//     await Future.delayed(const Duration(seconds: 5));
-//     await definition(word);
-//     await addToHistory(word);
-
-//     final rerun = stdin.readLineSync();
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-//   } else if (number == '3') {
-//     showHistory();
-//     print("");
-//     print("");
-//     print("Clear History");
-//     print("");
-//     print("");
-//     print("If you want to clear history type `Clear` in terminal");
-//     print("");
-//     print("");
-
-//     final rerun = stdin.readLineSync();
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-
-//     final delete = stdin.readLineSync();
-//     if (delete == "Clear") {
-//       deleteHistory();
-//     } else {
-//       print("You're good to go");
-//     }
-//   } else if (number == '4') {
-//     await about();
-
-//     final rerun = stdin.readLineSync();
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-//   } else if (number == '5') {
-//     await help();
-//     final rerun = stdin.readLineSync();
-
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-//   } else {
-//     print("Invalid choice. Please enter a number between 1 and 5.");
-
-//     final rerun = stdin.readLineSync();
-
-//     if (rerun == 'run') {
-//       return main();
-//     } else if (rerun == 'end') {
-//       exit(0);
-//     }
-//   }
-// }
